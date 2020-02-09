712addad3aa788b4882e1bc286482325965a264d
Mike Torres <apolloproductionsdj@gmail.com>
2020-02-09 22:10:51 +0000

    finished project

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -1,13 +1,13 @@
 $(function() {
-   
-
 
     // The taskHtml method takes in a JavaScript representation
     // of the task and produces an HTML representation using
     // <li> tags
     function taskHtml(task) {
       var checkedStatus = task.done ? "checked" : "";
-      var liElement = '<li><div class="view"><input class="toggle" type="checkbox"' +
+      var liClass = task.done ? "completed" : "";
+      var liElement = '<li id="listItem-' + task.id +'" class="' + liClass + '">' + 
+      '<div class="view"><input class="toggle" type="checkbox"' +
         " data-id='" + task.id + "'" +
         checkedStatus +
         '><label>' +
@@ -24,14 +24,21 @@ $(function() {
     function toggleTask(e) {
       var itemId = $(e.target).data("id");
 
+
       var doneValue = Boolean($(e.target).is(':checked'));
+
 
       $.post("/tasks/" + itemId, {
         _method: "PUT",
         task: {
           done: doneValue
         }
-      });
+      }).success(function(data) {
+        var liHtml = taskHtml(data);
+        var $li = $("#listItem-" + data.id);
+        $li.replaceWith(liHtml);
+        $('.toggle').change(toggleTask);
+      } );
     }
 
     $.get("/tasks").success( function( data ) {
@@ -47,8 +54,7 @@ $(function() {
 
     });
 
-
-     $('#new-form').submit(function(event) {
+    $('#new-form').submit(function(event) {
       event.preventDefault();
       var textbox = $('.new-todo');
       var payload = {
